2cc876dc4eb57d02e11615f526169ea7f12a2e2c
Manoel Mateus <mateus.david@outlook.com>
2017-10-19 13:44:54 -0200

    Organinzando o projeto

--- IoT_portao_automatico.ino
+++ IoT_portao_automatico.ino
@@ -1,23 +1,38 @@
+// ==========================================================================
+// INICIO: ESCOPO DAS VARIAVEIS
+
+
+// FIM:  ESCOPO DAS VARIAVEIS
+// ==========================================================================
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// ==========================================================================
+// INICIO: ESCOPO DAS FUNCOES
+
 /**
- * Função para desligar ou ligar um led
- * var porta  = Receberá a porta do led que a função irá trablhar
- * var acao   = Receberá a ação de desligar ou ligar o led, por exemplo, HIGH para ligar ou LOW para desligar
- * var atraso = Receberá o delay, caso seja nessessario, caso contrario só executara a ação 
- * exemplo: ligaDesligaLed(13, HIGH, 1000) irá ligar o led e deixando com um delay de 1 segundo para a proxima ação
+ * Descrição     = Função para desligar ou ligar um led.
+ * param porta   = Receberá a porta do led que a função irá trablhar.
+ * param acao    = Receberá a ação de desligar ou ligar o led, por exemplo, HIGH para ligar ou LOW para desligar.
+ * param atraso  = Receberá o delay, caso seja nessessario, caso contrario só executara a ação.
+ * param inverso = Receberá true caso queira que ele faça o inverso da primeira acao,delay obrigatorio.
+ * exemplo       = ligaDesligaLed(13, HIGH, 1000) irá ligar o led e deixando com um delay de 1 segundo para a proxima ação
  */
-void ligaDesligaLed(int porta, int acao , int atraso = 0) {
+void ligaDesligaLed(int porta, int acao, int atraso = 0, bool inverso = false) {
   digitalWrite(porta, acao);
   if (atraso != 0) {
     delay(atraso);
   }
+  if (inverso == true && atraso != 0) {
+    digitalWrite(porta, !digitalRead(porta));
+    delay(atraso);
+  }
 }
+// FIM: ESCOPO DAS FUNCOES
+// ==========================================================================
 
 void loop() {
-  ligaDesligaLed(13,HIGH,1000);
-  ligaDesligaLed(13,LOW,1000);
+  
 }
