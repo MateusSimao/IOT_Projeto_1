e9892a564f81313509553981b038d3cc355ae8c2
MANOEL MATEUS DAVID SIMÃO <42822663840@jpmorgan.sp>
2017-10-19 21:19:39 -0200

    'Atualizações'

--- IoT_portao_automatico/IoT_portao_automatico.ino
+++ IoT_portao_automatico/IoT_portao_automatico.ino
@@ -1,17 +1,48 @@
 // ==========================================================================
-// INICIO: ESCOPO DAS VARIAVEIS
+// INICIO: ESCOPO DAS BIBLIOTECAS
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <PubSubClient.h>
+#include <Servo.h>
 
 
-// FIM:  ESCOPO DAS VARIAVEIS
 // ==========================================================================
-
-void setup() {
-  
-  Serial.begin(9600);
-}
+// INICIO: ESCOPO DAS VARIAVEIS
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x62};                   // Definindo a variavel que recebe o mac address.
+void callback(char *topic, byte *payload, unsigned int length);      // Definindo a variavel que recebe o cabeçalho da função callback
+int pos;                                                             // Posição Servo
+Servo s;                                                             // Variável Servo
+EthernetClient ethClient;                                            // Definindo a variavel que recebe o cliente internet
+PubSubClient client("m10.cloudmqtt.com", 16815, callback, ethClient);// Dados do MQTT Cloud
+int ledAzul     = 5;
+int ledVermelho = 3;
+int ledVerde    = 4;
 
 // ==========================================================================
 // INICIO: ESCOPO DAS FUNCOES
+/** 
+ * Funcçao que irá receber o retorno do servidor. 
+ */
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char c = payload;
+  Serial.println(c);
+  Serial.println(topic);
+
+  if(strcmp("mensagem", topic) == 0){
+    Serial.println('a');
+    for(pos = 90; pos > 1; pos--)
+    {
+      s.write(pos);
+      delay(15);
+    }
+  }
+
+  byte *p = (byte *)malloc(length);
+  memcpy(p, payload, length);
+  free(p);
+}
 
 /**
  * Descrição     = Função para desligar ou ligar um led em portas digitais.
@@ -32,11 +63,41 @@ void ligaDesligaLedDigital(int porta, int acao, int atraso = 0, bool inverso = f
     delay(atraso);
   }
 }
-// FIM: ESCOPO DAS FUNCOES
-// ==========================================================================
 
-void loop() {
-  ligaDesligaLedDigital(3, HIGH, 500, true);
-  ligaDesligaLedDigital(4, HIGH, 500, true);
-  ligaDesligaLedDigital(5, HIGH, 500, true);
+void setup()
+{
+  //Servo
+  s.attach(6);
+  //s.write(0);
+  
+  Serial.begin(9600);
+  Serial.println("Iniciando...");
+  ligaDesligaLedDigital(ledAzul, HIGH);
+  Ethernet.begin(mac);  
+  // Faz a conexão no cloud com nome do dispositivo, usuário e senha respectivamente
+  if (client.connect("arduino", "test", "test"))
+  {
+    ligaDesligaLedDigital(ledAzul, LOW);
+    ligaDesligaLedDigital(ledVerde, HIGH);
+    
+    // Envia uma mensagem para o cloud no topic portao
+    client.publish("mensagem", 1);
+
+    // Conecta no topic para receber mensagens
+    client.subscribe("mensagem");
+    
+    Serial.println("conectado Temperatura");
+  }else{
+    ligaDesligaLedDigital(ledAzul, LOW);
+    ligaDesligaLedDigital(ledVerde, LOW);
+    ligaDesligaLedDigital(ledVermelho, HIGH);
+    Serial.println("erro ao conectar");  
+  }
+  
+}
+
+void loop()
+{
+  client.loop();
+  
 }
